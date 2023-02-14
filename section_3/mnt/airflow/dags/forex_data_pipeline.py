84b78666b775d3c8e81e84c7397905d533e09fd1
Lucas Neris <neris.lucas.222@gmail.com>
2023-02-14 20:28:47 -0300

    fix  is_forex_rates_available task

--- section_3/mnt/airflow/dags/forex_data_pipeline.py
+++ section_3/mnt/airflow/dags/forex_data_pipeline.py
@@ -17,7 +17,7 @@ with DAG('forex_data_pipeline', start_date=datetime(2021, 1, 1), schedule_interv
         task_id='is_forex_rates_available',
         http_conn_id='forex_api',
         endpoint='marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b',
-        response_check=lambda response: 'rates' in response.txt,
+        response_check=lambda response: 'rates' in response.text,
         poke_interval=5,
         timeout=20
     )
