c78315ea186c02e23d2693b2f4df431a3b7294ba
Lucas Neris <neris.lucas.222@gmail.com>
2023-02-14 20:47:54 -0300

    add  is_forex_currencies_file_available task

--- section_3/mnt/airflow/dags/forex_data_pipeline.py
+++ section_3/mnt/airflow/dags/forex_data_pipeline.py
@@ -1,7 +1,8 @@
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
-
-from datetime import datetime, timedelta
+from airflow.sensors.filesystem import FileSensor
 
 default_args = {
     "owner": "airflow",
@@ -12,7 +13,12 @@ default_args = {
     "retry_delay": timedelta(minutes=5)
 
 }
-with DAG('forex_data_pipeline', start_date=datetime(2021, 1, 1), schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
+
+with DAG('forex_data_pipeline',
+         start_date=datetime(2021, 1, 1),
+         schedule_interval="@daily",
+         default_args=default_args,
+         catchup=False) as dag:
     is_forex_rates_available = HttpSensor(
         task_id='is_forex_rates_available',
         http_conn_id='forex_api',
@@ -20,4 +26,11 @@ with DAG('forex_data_pipeline', start_date=datetime(2021, 1, 1), schedule_interv
         response_check=lambda response: 'rates' in response.text,
         poke_interval=5,
         timeout=20
+    )
+
+    is_forex_currencies_file_available = FileSensor(
+        task_id='is_forex_currencies_file_available',
+        fs_conn_id='forex_path',
+        filepath='forex_currencies.csv',
+        poke_interval=5,
     )
