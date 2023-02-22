72edb7f045678a72f8619a7ab64ace31f5b43035
Lucas Neris <neris.lucas.222@gmail.com>
2023-02-22 16:07:59 -0300

    add downloading_rates task

--- section_3/mnt/airflow/dags/forex_data_pipeline.py
+++ section_3/mnt/airflow/dags/forex_data_pipeline.py
@@ -1,6 +1,10 @@
+import csv
+import json
 from datetime import datetime, timedelta
 
+import requests
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
 
@@ -11,8 +15,32 @@ default_args = {
     "email": "admin@localhost.com",
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
-
 }
+
+# Download forex rates according to the currencies we want to watch
+# described in the fsile forex_currencies.csv
+
+
+def download_rates():
+    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+    ENDPOINTS = {
+        'USD': 'api_forex_exchange_usd.json',
+        'EUR': 'api_forex_exchange_eur.json'
+    }
+    with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
+        reader = csv.DictReader(forex_currencies, delimiter=';')
+        for idx, row in enumerate(reader):
+            base = row['base']
+            with_pairs = row['with_pairs'].split(' ')
+            indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
+            outdata = {'base': base, 'rates': {},
+                       'last_update': indata['date']}
+            for pair in with_pairs:
+                outdata['rates'][pair] = indata['rates'][pair]
+            with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
+                json.dump(outdata, outfile)
+                outfile.write('\n')
+
 
 with DAG('forex_data_pipeline',
          start_date=datetime(2021, 1, 1),
@@ -33,4 +61,9 @@ with DAG('forex_data_pipeline',
         fs_conn_id='forex_path',
         filepath='forex_currencies.csv',
         poke_interval=5,
+    )
+
+    downloading_rates = PythonOperator(
+        task_id='downloading_rates',
+        python_callable=download_rates
     )
